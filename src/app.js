3439488d3b45746d89c22e863abd847ce624dc34
marenoztro <marlon.marotto@gmail.com>
2022-11-28 13:06:45 +0000

    okok listo

--- src/app.js
+++ src/app.js
@@ -5,44 +5,6 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// let values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "k"];
-// let images = ["♦", "♥", "♠", "♣"];
-// let variable_values = 0;
-// let variables_images = 0;
-
-// window.onload = function() {
-//   //write your code here
-
-//   variable_values = [Math.floor(Math.random() * values.length)];
-//   variables_images = [Math.floor(Math.random() * images.length)];
-
-//   let header = document.querySelector("#header");
-//   let center = document.querySelector("#center");
-//   let footer = document.querySelector("#footer");
-
-//   header.innerHTML = images[variables_images];
-//   center.innerHTML = values[variable_values];
-//   footer.innerHTML = images[variables_images];
-// };
-
-// onload();
-
-// let cartas = ["A", 2, 3, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-// let simbolos = ["♦", "♥", "♠", "♣"];
-
-// window.onload = function() {
-//   //write your code here
-
-//   let cartasRandom = Math.floor(Math.random() * cartas.lenght); // OJO: acá lo que hacemos es generar el número aleatorio de las Cartas.
-//   //console.log(cartas[cartasRandom]); // OJO: acá lo que hacemos es llamar al array cartas en la posición cardRandom.
-
-//   let simbolosRandom = Math.floor(Math.random() * simbolos.lenght); // OJO: acá lo que hacemos es generar el número aleatorio de los símbolos.
-//   //console.log(simbolos[simbolosRandom]); // OJO: acá lo que hacemos es llamar al array simbolos en la posición simbolosRandom.
-
-//   let header = document.querySelector("#header");
-//   header.innerHTML = simbolos[simbolosRandom];
-// };
-
 const suit = ["♦", "♥", "♠", "♣"];
 
 const number = [
@@ -86,3 +48,41 @@ window.onload = () => {
 };
 
 onload();
+
+// let values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "k"];
+// let images = ["♦", "♥", "♠", "♣"];
+// let variable_values = 0;
+// let variables_images = 0;
+
+// window.onload = function() {
+//   //write your code here
+
+//   variable_values = [Math.floor(Math.random() * values.length)];
+//   variables_images = [Math.floor(Math.random() * images.length)];
+
+//   let header = document.querySelector("#header");
+//   let center = document.querySelector("#center");
+//   let footer = document.querySelector("#footer");
+
+//   header.innerHTML = images[variables_images];
+//   center.innerHTML = values[variable_values];
+//   footer.innerHTML = images[variables_images];
+// };
+
+// onload();
+
+// let cartas = ["A", 2, 3, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+// let simbolos = ["♦", "♥", "♠", "♣"];
+
+// window.onload = function() {
+//   //write your code here
+
+//   let cartasRandom = Math.floor(Math.random() * cartas.lenght); // OJO: acá lo que hacemos es generar el número aleatorio de las Cartas.
+//   //console.log(cartas[cartasRandom]); // OJO: acá lo que hacemos es llamar al array cartas en la posición cardRandom.
+
+//   let simbolosRandom = Math.floor(Math.random() * simbolos.lenght); // OJO: acá lo que hacemos es generar el número aleatorio de los símbolos.
+//   //console.log(simbolos[simbolosRandom]); // OJO: acá lo que hacemos es llamar al array simbolos en la posición simbolosRandom.
+
+//   let header = document.querySelector("#header");
+//   header.innerHTML = simbolos[simbolosRandom];
+// };
